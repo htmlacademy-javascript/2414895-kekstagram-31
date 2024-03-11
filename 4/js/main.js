a6c955a4fac50ea16c70a597f7fd3d63268c9095
Keks <keks@htmlacademy.ru>
2024-03-11 23:34:54 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,3 @@
+import {createPhotosDescription} from './data.js';
+
+createPhotosDescription();
---
+++ 4/js/util.js
@@ -0,0 +1,26 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(min, max));
+  const upper = Math.floor(Math.max(min, max));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createRandomIdFromRangeGenerator = () => {
+  const previousValues = [];
+
+  return function (element) {
+    let currentValue = getRandomInteger(element.MIN, element.MAX);
+    if (previousValues.length >= (element.MAX - element.MIN + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(element.MIN, element.MAX);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export {getRandomInteger, createRandomIdFromRangeGenerator, getRandomArrayElement};
